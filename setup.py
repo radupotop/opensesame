4c1551260e8235bf68b5361970b00bb46b5604c1
Radu Potop <radu@wooptoo.com>
2020-02-13 23:31:06 +0000

    Add setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,16 @@
+from pathlib import Path
 from setuptools import setup, find_packages
 
-setup(name='OpenSesame', version='0.1', packages=find_packages())
+
+def read_requirements(filename):
+    contents = Path(filename).read_text().strip('\n')
+    return [line for line in contents.split('\n') if not line.startswith('-')]
+
+
+setup(
+    name='OpenSesame',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=read_requirements('requirements.txt'),
+    extras_require=dict(tests=read_requirements('requirements.test.txt')),
+)
