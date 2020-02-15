de7262bbb8157f9c0037b1f3c806dafde3dccfb2
Radu Potop <radu@wooptoo.com>
2020-02-15 22:44:44 +0000

    Test expired tokens

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 import pytest
 
 from app.storage import Storage
@@ -26,3 +27,23 @@ class TestToken:
         token, expires = new_token
         assert not storage.verify_token(token)
         assert not storage.expire_token(token)
+
+
+class TestTokenVerifyExpired:
+    @pytest.fixture(scope='class')
+    def new_token(self):
+        return storage.add_token(expiry_days=3)
+
+    def test_add_token(self, new_token):
+        token, expires = new_token
+        assert token
+        assert expires > datetime.utcnow()
+
+    def test_verify_token_valid(self, new_token):
+        token, expires = new_token
+        assert storage.verify_token(token)
+
+    @pytest.mark.freeze_time(datetime.utcnow() + timedelta(days=4))
+    def test_verify_token_expired(self, new_token):
+        token, expires = new_token
+        assert not storage.verify_token(token)
