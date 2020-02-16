08820c5debbd23d31d47469638df4f9f67f80c2d
Radu Potop <radu@wooptoo.com>
2020-02-16 00:57:40 +0000

    Basic token verify

--- app/api.py
+++ app/api.py
@@ -4,6 +4,10 @@ from time import sleep
 from werkzeug.serving import run_simple
 from werkzeug.wrappers import Request, Response
 
+from app.storage import Storage
+
+storage = Storage()
+
 
 @Request.application
 def application(request):
@@ -11,7 +15,7 @@ def application(request):
     token = request.args.get('token')
     src_ip = request.host
 
-    if token:
+    if token and storage.verify_token(token):
         return Response('"OK"', content_type='application/json', status=200)
     else:
         return Response('"ERR"', content_type='application/problem+json', status=400)
