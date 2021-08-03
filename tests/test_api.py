cdeaf4a2c99f95aa727ed5e28d1a63b9e58c2fb0
Radu Potop <radu@wooptoo.com>
2021-08-03 20:27:37 +0100

    Added API tests

---
+++ tests/test_api.py
@@ -0,0 +1,30 @@
+import pytest
+from api.api import application
+from app.config import ConfigReader
+from app.iptables import IPTables
+from app.storage import Storage
+from utils.bootstrap import create_db
+from werkzeug.test import Client
+
+create_db()
+storage = Storage()
+
+
+class TestAPI:
+    @pytest.fixture(scope='class')
+    def new_token(self):
+        return storage.add_token()
+
+    def setup_class(self):
+        self.client = Client(application)
+        # self.cfg = ConfigReader()
+        # self.ipt = IPTables(self.cfg)
+
+    def test_valid_request(self, new_token):
+        token_value, expires = new_token
+        response = self.client.get(f'/?token={token_value}')
+        assert response.status_code == 201
+
+    def test_bad_request(self):
+        response = self.client.get(f'/?token=BADTOKEN')
+        assert response.status_code == 403
