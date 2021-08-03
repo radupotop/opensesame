93b6f1c4fbb0eb7bdb42b1da7cc4f7e8de340cad
Radu Potop <radu@wooptoo.com>
2021-08-03 22:08:38 +0100

    Fixed API tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,25 +1,37 @@
+import iptc
 import pytest
+from werkzeug.test import Client
+
 from api.api import application
+from app.config import ConfigReader
+from app.iptables import IPTables
 from app.storage import Storage
 from utils.bootstrap import create_db
-from werkzeug.test import Client
 
 create_db()
 storage = Storage()
 
 
 class TestAPI:
-    @pytest.fixture(scope='class')
-    def new_token(self):
-        return storage.add_token()
-
     def setup_class(self):
+        cfg = ConfigReader()
+        ipt = IPTables(cfg)
+        ipt.setup_whitelist_chain()
         self.client = Client(application)
+        self.token = storage.add_token()
 
-    def test_valid_request(self, new_token):
-        token_value, expires = new_token
+    def teardown_class(self):
+        iptc.easy.delete_chain('filter', 'opensesame', flush=True)
+
+    def test_valid_request(self):
+        token_value, expires = self.token
         response = self.client.get(f'/?token={token_value}')
         assert response.status_code == 201
+
+    def test_valid_request_existing_ip(self):
+        token_value, expires = self.token
+        response = self.client.get(f'/?token={token_value}')
+        assert response.status_code == 200
 
     def test_bad_request(self):
         response = self.client.get(f'/?token=BADTOKEN')
