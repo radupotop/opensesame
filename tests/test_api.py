9f7c1055c075ce9f9364efa0883ffc9a4e97df25
Radu Potop <radu@wooptoo.com>
2024-04-05 01:01:09 +0100

    Fix tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,5 +1,4 @@
 import iptc
-import pytest
 from werkzeug.test import Client
 
 from api.api import application
@@ -14,14 +13,14 @@ storage = Storage()
 
 class TestAPI:
     def setup_class(self):
-        cfg = ConfigReader()
-        ipt = IPTables(cfg)
+        self.cfg = ConfigReader()
+        ipt = IPTables(self.cfg)
         ipt.setup_whitelist_chain()
         self.client = Client(application)
         self.token = storage.add_token()
 
     def teardown_class(self):
-        iptc.easy.delete_chain('filter', 'opensesame', flush=True)
+        iptc.easy.delete_chain(iptc.Table.FILTER, self.cfg.chain, flush=True)
 
     def test_valid_request(self):
         token_value, expires = self.token
