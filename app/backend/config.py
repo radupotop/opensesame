a4e6593632853a42f9201295b390fb142ff05cc1
Radu Potop <radu@wooptoo.com>
2024-04-10 21:52:17 +0100

    Add YamlConfigReader

--- app/backend/config.py
+++ app/backend/config.py
@@ -3,9 +3,13 @@ from pathlib import Path
 import yaml
 
 
-class ConfigReader:
-    def __init__(self):
-        raw_yaml = Path('config.yml').read_text()
+class YamlConfigReader:
+    def __init__(self, filename='config.yml'):
+        raw_yaml = Path(filename).read_text()
         config_file = yaml.safe_load(raw_yaml)
         for k, v in config_file.items():
             setattr(self, str(k), v)
+
+
+class ConfigReader(YamlConfigReader):
+    pass
